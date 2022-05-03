666814448f985cf7c930a96efafcdae87cac99c9
Ben Carlson <bjcarlson42@outlook.com>
2022-05-03 19:15:43 -0400

    shared preferences

--- app/src/main/java/com/example/finalproject/DrinkAdapter.kt
+++ app/src/main/java/com/example/finalproject/DrinkAdapter.kt
@@ -6,9 +6,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.drink.view.*
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_three.*
+import java.lang.reflect.Type
 
 class DrinkAdapter(val context: Context, val item: List<Drink>) : RecyclerView.Adapter<DrinkAdapter.ViewHolder>(){
 
@@ -24,6 +30,52 @@ class DrinkAdapter(val context: Context, val item: List<Drink>) : RecyclerView.A
     override fun getItemCount() = item.size
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        init {
+            // on long press, save the drink
+            itemView.setOnLongClickListener {
+                val drinkToAdd = item[adapterPosition].drink_name
+                Toast.makeText(itemView.context, "Saved $drinkToAdd", Toast.LENGTH_LONG).show()
+
+                var l = getArrayList("favs")
+                if (l != null) {
+                    l.add(drinkToAdd)
+                    saveArrayList(l, "favs")
+                } else {
+                    var list = ArrayList<String?>()
+                    list.add(drinkToAdd)
+                    saveArrayList(list, "favs")
+                }
+
+                return@setOnLongClickListener true
+            }
+        }
+
+        val FILE_NAME = "favList"
+        // helper function to save https://stackoverflow.com/questions/7057845/save-arraylist-to-sharedpreferences
+        fun saveArrayList(list: java.util.ArrayList<String?>?, key: String?) {
+            val prefs = itemView.context.getSharedPreferences(FILE_NAME,
+                AppCompatActivity.MODE_PRIVATE
+            )
+            val editor = prefs.edit()
+            val gson = Gson()
+            val json: String = gson.toJson(list)
+            editor.putString(key, json)
+            editor.apply()
+        }
+
+        // helper function to get https://stackoverflow.com/questions/7057845/save-arraylist-to-sharedpreferences
+        fun getArrayList(key: String?): java.util.ArrayList<String?>? {
+            val prefs = itemView.context.getSharedPreferences(FILE_NAME,
+                AppCompatActivity.MODE_PRIVATE
+            )
+            val gson = Gson()
+            val json: String? = prefs.getString(key, null)
+            val type: Type = object : TypeToken<java.util.ArrayList<String?>?>() {}.getType()
+            return gson.fromJson(json, type)
+        }
+
+
         fun bind(d: Drink) {
             // set the values in the view to the actual values
             itemView.drink_name.text = d.drink_name
